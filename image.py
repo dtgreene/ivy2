a65e15d7a3a3e2f962ea0e5bf0f6f967780848cc
dtgreene <dylantgreene92@gmail.com>
2023-04-10 16:44:58 +0100

    More tinkering

--- image.py
+++ image.py
@@ -1,8 +1,12 @@
 from PIL import Image
 from io import BytesIO
 
-PRINT_OUT_WIDTH = 1280
-PRINT_OUT_HEIGHT = 1920
+PRINT_START_WIDTH = 1280
+PRINT_START_HEIGHT = 1920
+
+PRINT_FINAL_WIDTH = 640
+PRINT_FINAL_HEIGHT = 1616
+
 
 def prepare_image(path, quality=100):
     image = Image.open(path, "r")
@@ -12,12 +16,12 @@ def prepare_image(path, quality=100):
     scaled_width = width
     scaled_height = height
 
-    if scaled_width < PRINT_OUT_WIDTH:
-        scaled_width = PRINT_OUT_WIDTH
+    if scaled_width < PRINT_START_WIDTH:
+        scaled_width = PRINT_START_WIDTH
         scaled_height = int(scaled_width * ratio)
 
-    if scaled_height < PRINT_OUT_HEIGHT:
-        scaled_height = PRINT_OUT_HEIGHT
+    if scaled_height < PRINT_START_HEIGHT:
+        scaled_height = PRINT_START_HEIGHT
         scaled_width = int(scaled_height / ratio)
 
     if scaled_width != width or scaled_height != height:
@@ -26,15 +30,18 @@ def prepare_image(path, quality=100):
         )
 
     offset = (
-        (PRINT_OUT_WIDTH - scaled_width) // 2,
-        (PRINT_OUT_HEIGHT - scaled_height) // 2
+        (PRINT_START_WIDTH - scaled_width) // 2,
+        (PRINT_START_HEIGHT - scaled_height) // 2
     )
 
-    out_image = Image.new("RGB", (PRINT_OUT_WIDTH, PRINT_OUT_HEIGHT))
+    out_image = Image.new("RGB", (PRINT_START_WIDTH, PRINT_START_HEIGHT))
     out_image.paste(image, offset)
 
+    out_image = out_image.resize((PRINT_FINAL_WIDTH, PRINT_FINAL_HEIGHT))
+    out_image = out_image.rotate(180.0)
+
     # debug
-    # out_image.save("out.jpeg", format="JPEG", quality=90)
+    # out_image.save("out.jpeg", format="JPEG", quality=quality)
 
     with BytesIO() as out_stream:
         out_image.save(out_stream, format="JPEG", quality=quality)
